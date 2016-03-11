15c1dcfb869222bb8cff1561050feb9923640275
wangqing <wangqing@mail.39.net>
2016-03-11 17:25:07 +0800

    消息管理

--- wx/wx-dto/src/java/com/dto/wx/InputMessageDto.java
+++ wx/wx-dto/src/java/com/dto/wx/InputMessageDto.java
@@ -1,13 +1,68 @@
 package com.dto.wx;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import com.utils.XmlParseUtils;
+import org.dom4j.Document;
+
 import java.io.Serializable;
 
 /**
  * Created by Admin on 2016/3/6.
- * 公众账号发消息
+ * 公众号消息报文
  */
-@XmlRootElement()
 public class InputMessageDto implements Serializable {
+    private Document doc;
+    public InputMessageDto(Document doc){
+        this.doc=doc;
+    }
 
+    /**
+     * 获取信息接收者Openid
+     * @return
+     */
+    public String getToUserName(){
+        String str= XmlParseUtils.getDocElementTextByPath(doc,"xml/ToUserName");
+        return str;
+    }
+
+    /**
+     * 消息发送者Openid
+     * @return
+     */
+    public String getFromUserName(){
+        return XmlParseUtils.getDocElementTextByPath(doc,"xml/FromUserName");
+    }
+
+    /**
+     * 回复时间
+     * @return
+     */
+    public Long getCreateTime(){
+        String str=XmlParseUtils.getDocElementTextByPath(doc,"xml/CreateTime");
+        return Long.parseLong(str);
+    }
+
+    /**
+     *消息类型
+     * @return
+     */
+    public String getMsgType(){
+        return XmlParseUtils.getDocElementTextByPath(doc,"xml/MsgType");
+    }
+
+    /**
+     * 消息内容
+     * @return
+     */
+    public String getContent(){
+        return XmlParseUtils.getDocElementTextByPath(doc,"xml/Content");
+    }
+
+    /**
+     * 消息id
+     * @return
+     */
+    public int getMsgId(){
+        String str=XmlParseUtils.getDocElementTextByPath(doc,"xml/MsgId");
+        return Integer.parseInt(str);
+    }
 }
